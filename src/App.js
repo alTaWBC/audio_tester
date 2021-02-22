620f63fa1e5daa84837af358eff3302a948bc3f2
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-02-22 15:16:00 +0000

    New Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        soundBytes: [],
+        sounds: [],
+        index: null,
+    };
+
+    componentDidMount() {
+        this.preparingMicrophone();
+    }
+
+    preparingMicrophone = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        this.recorder = new MediaRecorder(stream);
+        this.recorder.addEventListener("dataavailable", this.onDataAvailable);
+        this.recorder.addEventListener("stop", this.onStop);
+    };
+
+    onStop = ({ data }) => {
+        let soundBytes = [...this.state.soundBytes, data];
+        const blob = new Blob(soundBytes);
+        const url = URL.createObjectURL(blob);
+        const sound = new Audio(url);
+        const sounds = [...this.state.sounds, sound];
+        soundBytes = [];
+        this.setState({ sounds, soundBytes });
+    };
+
+    onDataAvailable = ({ data }) => {
+        const soundBytes = [...this.state.soundBytes, data];
+        this.setState({ soundBytes });
+    };
+
+    selectAudio = (index) => this.setState({ index });
+
+    createAudios = (audios) => {
+        return audios.map((_, index) => {
+            return (
+                <div key={index}>
+                    <button onClick={() => this.selectAudio(index)}>{index}</button>
+                </div>
+            );
+        });
+    };
+
+    playAudio = () => {
+        const noAudioWasSelected = this.state.index === null;
+        if (noAudioWasSelected) return;
+
+        this.state.sounds[this.state.index].play();
+    };
+
+    recordAudio = () => {
+        const MicrophoneIsNotInitialized = this.recorder === null;
+        if (MicrophoneIsNotInitialized) return;
+
+        const MicrophoneIsRecording = this.recorder.state === "recording";
+        if (MicrophoneIsRecording) {
+            this.recorder.stop();
+        } else {
+            this.recorder.start();
+        }
+    };
+
+    render() {
+        const audios = this.createAudios(this.state.sounds);
+        return (
+            <div>
+                <button onClick={this.recordAudio}>{this.recorder?.state === "recording" ? "Stop" : "Start"}</button>
+                {audios}
+                {this.state.index}
+                <button onClick={this.playAudio} disabled={this.state.index === null}>
+                    {this.state.index !== null ? `Play ${this.state.index}` : "Select Sound"}
+                </button>
+            </div>
+        );
+    }
 }
 
 export default App;
